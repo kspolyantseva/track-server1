349700bc8b8966e74a2397b76dea3f181ee6e7ec
Svetlana <malyshkakivi@yandex.ru>
2017-11-21 01:07:14 +0300

    auth changes

--- public/login.js
+++ public/login.js
@@ -5,6 +5,7 @@ exports.post = function(req, res, next) {
     var password = req.body.password;
 
     User.authorize(username, password, function(err, user) {
+      if(err) return res.send(400,err);
       req.session.regenerate(function(err) {
         if(!err){req.session.userName=user.name;
           req.session.UserId=user._id;
