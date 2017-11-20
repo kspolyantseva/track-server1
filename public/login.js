456f85e10aa946e3cb85124b137f1bb1c7693ae8
Svetlana <malyshkakivi@yandex.ru>
2017-11-21 01:22:38 +0300

    auth change3

--- public/login.js
+++ public/login.js
@@ -5,7 +5,7 @@ exports.post = function(req, res, next) {
     var password = req.body.password;
 
     User.authorize(username, password, function(err, user) {
-      if(err) return res.send(400,err);
+      if(err) return res.status(400).send('user not found');
       req.session.regenerate(function(err) {
         if(!err){req.session.userName=user.name;
           req.session.UserId=user._id;
